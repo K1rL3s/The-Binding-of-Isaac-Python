d80a5ea21497f4c8a441ba0ea2ef5d3947915e7c
kiril <kirill.lesovoi@mail.ru>
2022-12-26 00:44:41 +0500

    feat(main): Добавил пробный запуск модулей

--- main.py
+++ main.py
@@ -1,9 +1,59 @@
-import pygame
+import pygame as pg
+
+from src import consts
+from src.utils.funcs import load_image, load_sound
+from src.modules.levels.Room import Room
 
 
 def main():
-    pygame.init()
-    pygame.quit()
+    pg.init()
+    pg.font.init()
+    pg.mixer.init()
+    screen = pg.display.set_mode((consts.WIDTH, consts.HEIGHT))
+
+    all_textures = {
+        "rooms": {
+            "controls": pg.Surface((0, 0)),
+            consts.FloorsTypes.BASEMENT: load_image("textures/room/basement.png"),
+            consts.FloorsTypes.CAVES: load_image("textures/room/basement.png"),
+            consts.FloorsTypes.CATACOMBS: load_image("textures/room/basement.png"),
+            consts.FloorsTypes.DEPTHS: load_image("textures/room/basement.png"),
+            consts.FloorsTypes.BLUEWOMB: load_image("textures/room/basement.png"),
+            consts.FloorsTypes.WOMB: load_image("textures/room/basement.png"),
+            consts.RoomsTypes.TREASURE: load_image("textures/room/basement.png"),
+            consts.RoomsTypes.SHOP: load_image("textures/room/basement.png"),
+            consts.RoomsTypes.SECRET: load_image("textures/room/basement.png"),
+        },
+        "rocks": {
+            "alive": load_image("textures/room/rocks.png"),
+            "destroy_animation": None,
+        }
+    }
+
+    all_sounds = {
+        "fart": load_sound("sounds/fart.mp3"),
+        "rock_crumble": [
+            load_sound("sounds/rock_crumble1.wav"),
+            load_sound("sounds/rock_crumble2.wav"),
+            load_sound("sounds/rock_crumble3.wav"),
+        ],
+    }
+
+    room = Room(consts.FloorsTypes.WOMB, consts.RoomsTypes.DEFAULT, 1, (0, 0), None, all_textures, all_sounds)
+
+    running = True
+    timer = pg.time.Clock()
+
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+        delta_t = timer.tick(60) / 1000
+        screen.fill(pg.Color('white'))
+        room.render(screen)
+        pg.display.flip()
+
+    pg.quit()
 
 
 if __name__ == '__main__':
---
+++ src/utils/funcs.py
@@ -0,0 +1,41 @@
+import os
+
+import pygame as pg
+
+
+def load_image(name: str, colorkey: pg.Color | int | None = None):
+    """
+    Загрузка изображения в pygame.Surface.
+
+    :param name: Путь до файла, начиная от src/data, e.g. "textures/room/basement.png"
+    :param colorkey: Пиксель, по которому будет удаляться задний фон. Если -1, то по левому верхнему.
+    :return: pygame.Surface
+    """
+    fullname = os.path.join('src', 'data', name.replace('/', '\\'))
+    if not os.path.isfile(fullname):
+        exit(f"Файл с изображением '{fullname}' не найден")
+    image = pg.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def load_sound(name):
+    """
+    Загрузка звука в pygame.Sound.
+
+    :param name: Путь до файла, начиная от src/data, e.g. "sounds/fart.mp23"
+    :return: pygame.Sound
+    """
+    fullname = os.path.join('src', 'data', name.replace('/', '\\'))
+    if not os.path.isfile(fullname):
+        exit(f"Файл с звуком '{fullname}' не найден")
+    sound = pg.mixer.Sound(fullname)
+    return sound
+
+
