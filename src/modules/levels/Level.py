bada551f3f2757812ea33ca9dc887b39bb733e5d
RostyanYandexLyceumCheeeeck <117914489+RostyanYandexLyceumCheeeeck@users.noreply.github.com>
2023-01-11 22:55:31 +0500

    Update Level.py
    фикс бага ( добавил 50 и 127 строчку )

--- src/modules/levels/Level.py
+++ src/modules/levels/Level.py
@@ -1,17 +1,17 @@
 import pygame as pg
 
-from src.utils.graph import valid_coords
+from src import consts
+from src.utils.graph import valid_coords, get_neighbors_coords
 from src.modules.levels.Room import Room
-from src.utils.graph import get_neighbors_coords
 from src.modules.levels.LevelGenerator import generate_level
-from src.consts import RoomsTypes, FloorsTypes, DoorsCoords, Moves
-
-from src.modules.BaseClasses.BaseEnemy import BaseEnemy
+from src.modules.animations.MovingRoomAnimation import MovingRoomAnimation
+from src.modules.BaseClasses import BaseEnemy
+from src.modules.characters.parents import Player
 
 
 class Level:
     """
-    Класс уровня/'этажа'.
+    Класс уровня/этажа.
 
     :param floor_type: Тип этажа.
     :param main_hero: Главный персонаж.
@@ -19,17 +19,18 @@ class Level:
     :param height: Максимальная высота расстановки комнат.
     """
     def __init__(self,
-                 floor_type: FloorsTypes | str,
-                 main_hero: BaseEnemy,
+                 floor_type: consts.FloorsTypes | str,
+                 main_hero: Player,
                  width: int = 10,
                  height: int = 6):
         self.floor_type = floor_type
         self.main_hero = main_hero
         self.width = width
         self.height = height
-        self.level_map: list[list[RoomsTypes | str]] = []
+        self.level_map: list[list[consts.RoomsTypes | str]] = []
         self.rooms: list[list[Room | None]] = [[None] * width for _ in range(height)]
         self.current_room: Room | None = None
+        self.is_moving: bool | MovingRoomAnimation = False
 
         self.setup_level()
 
@@ -40,20 +41,22 @@ class Level:
         self.level_map = generate_level(self.width, self.height, 15)
         for y, row in enumerate(self.level_map):
             for x, room_type in enumerate(row):
-                if room_type == RoomsTypes.EMPTY:
+                if room_type == consts.RoomsTypes.EMPTY:
                     continue
                 room = Room(self.floor_type, room_type, (x, y), self.main_hero, None)
                 room.setup_doors(self.get_doors(x, y))
-                if room_type == RoomsTypes.SPAWN:
+                if room_type == consts.RoomsTypes.SPAWN:
                     self.current_room = room
+                    self.main_hero.update_room_groups(self.current_room.get_room_groups())
+
                 # room.update_doors('blow')
                 # room.update_doors('open')
                 # room.update_doors('close')
                 self.rooms[y][x] = room
-        assert self.current_room is not None and self.current_room.room_type == RoomsTypes.SPAWN
+        assert self.current_room is not None and self.current_room.room_type == consts.RoomsTypes.SPAWN
         self.change_rooms_state(self.current_room.x, self.current_room.y)
 
-    def get_doors(self, cur_x: int, cur_y: int) -> list[tuple[DoorsCoords, RoomsTypes]]:
+    def get_doors(self, cur_x: int, cur_y: int) -> list[tuple[consts.DoorsCoords, consts.RoomsTypes]]:
         """
         Получение координат дверей и необходимой информации для установки текстурки.
 
@@ -66,18 +69,18 @@ class Level:
         for room_x, room_y in coords:
             direction = None
             if room_x > cur_x:
-                direction = DoorsCoords.RIGHT
+                direction = consts.DoorsCoords.RIGHT
             elif room_x < cur_x:
-                direction = DoorsCoords.LEFT
+                direction = consts.DoorsCoords.LEFT
             elif room_y > cur_y:
-                direction = DoorsCoords.DOWN
+                direction = consts.DoorsCoords.DOWN
             elif room_y < cur_y:
-                direction = DoorsCoords.UP
+                direction = consts.DoorsCoords.UP
             assert direction is not None, f'Не удалось получить расположение двери'
 
-            if (self.level_map[cur_y][cur_x] in
-                    (RoomsTypes.TREASURE, RoomsTypes.BOSS, RoomsTypes.SECRET, RoomsTypes.SHOP) and
-                    self.level_map[room_y][room_x] != RoomsTypes.SECRET):
+            if (self.level_map[cur_y][cur_x] in (consts.RoomsTypes.TREASURE, consts.RoomsTypes.BOSS,
+                                                 consts.RoomsTypes.SECRET, consts.RoomsTypes.SHOP)
+                    and self.level_map[room_y][room_x] != consts.RoomsTypes.SECRET):
                 doors.append((direction, self.level_map[cur_y][cur_x]))
             else:
                 doors.append((direction, self.level_map[room_y][room_x]))
@@ -102,25 +105,51 @@ class Level:
         for x, y in coords:
             self.rooms[y][x].update_detection_state(is_spotted=True)
 
-        if self.current_room.room_type == RoomsTypes.SECRET:
+        if self.current_room.room_type == consts.RoomsTypes.SECRET:
             self.current_room.update_doors("blow", with_sound=False)
 
-    def move_to_next_room(self, direction: Moves | tuple[int, int]):
+    def move_to_next_room(self, direction: consts.Moves):
         """
         Вход в другую комнату.
+
         :param direction: Направление движения.
         """
         x, y = direction.value
-        x = self.current_room.x + x
-        y = self.current_room.y + y
+        x, y = self.current_room.x + x, self.current_room.y + y
+
         if valid_coords(x, y, self.width, self.height) and self.rooms[y][x]:
+
+            self.is_moving = True
+            from_room = self.current_room
+            to_room = self.rooms[y][x]
+            self.moving_room_animation(from_room, to_room, direction)
+########################
+            self.main_hero.update_room_groups(self.current_room.get_room_groups())
+########################
             self.current_room = self.rooms[y][x]
             self.current_room.update_detection_state(is_active=True)
-            # self.current_room.update_doors("open")
             self.change_rooms_state(x, y)
 
+    def moving_room_animation(self, from_room: Room, to_room: Room, direction: consts.Moves):
+        """
+        Старт анимации перехода между комнатами.
+
+        :param from_room: Из какой комнаты.
+        :param to_room: В какую комнату.
+        :param direction: Направление.
+        """
+        self.is_moving = MovingRoomAnimation(from_room, to_room, direction)
+
     def update(self, delta_t: float):
-        self.current_room.update(delta_t)
+        if self.is_moving:
+            self.is_moving.update(delta_t)
+            if self.is_moving.is_over:
+                self.is_moving = False
+        else:
+            self.current_room.update(delta_t)
 
     def render(self, screen: pg.Surface):
-        self.current_room.render(screen)
+        if self.is_moving:
+            self.is_moving.render(screen)
+        else:
+            self.current_room.render(screen)
